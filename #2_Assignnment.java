dd689edbd7f3e50dd9ac082e79916c3023a29bb4
Sahil Gupta <113085266+sahil8017@users.noreply.github.com>
2025-02-22 21:49:59 +0530

    #2_Assignnment.java

---
+++ #2_Assignnment.java
@@ -0,0 +1,11 @@
+/*
+W.A.P that prints the following line on the screen along with quotes.
+
+    "Can we print '\' with System.out.println() statement?"
+
+*/
+class Printing {
+    public static void main(String args[]) {
+        System.out.println("Can we print '\\' with System.out.println() statement?");
+    }
+}
