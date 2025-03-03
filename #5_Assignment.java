a93a77cfba53820c8189be7596ccbe43ee650d76
Sahil Gupta <113085266+sahil8017@users.noreply.github.com>
2025-03-04 00:15:17 +0530

    #5_Assignment.java

---
+++ #5_Assignment.java
@@ -0,0 +1,75 @@
+import java.lang.reflect.Constructor;
+import java.lang.reflect.Field;
+
+public class multipleclasses {
+    public static void main(String args[]) {
+        student s1 = createStudent("Sahil", 20);
+        subject sb1 = createSubject("Computer", "Mathematics");
+
+        s1.display();
+        sb1.display();
+    }
+
+    static student createStudent(String name, int rollno) {
+        student s = null;
+        try {
+            Class<?> objClass = Class.forName("multipleclasses$student");
+            Constructor<?> constructor = objClass.getConstructor();
+            Object obj = constructor.newInstance();
+
+            Field nameField = objClass.getDeclaredField("name");
+            Field rollField = objClass.getDeclaredField("rollno");
+
+            nameField.setAccessible(true);
+            rollField.setAccessible(true);
+
+            nameField.set(obj, name);
+            rollField.set(obj, rollno);
+
+            s = (student) obj;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return s;
+    }
+
+    static subject createSubject(String sub1, String sub2) {
+        subject sb = null;
+        try {
+            Class<?> objClass = Class.forName("multipleclasses$subject");
+            Constructor<?> constructor = objClass.getConstructor();
+            Object obj = constructor.newInstance();
+
+            Field sub1Field = objClass.getDeclaredField("sub1");
+            Field sub2Field = objClass.getDeclaredField("sub2");
+
+            sub1Field.setAccessible(true);
+            sub2Field.setAccessible(true);
+
+            sub1Field.set(obj, sub1);
+            sub2Field.set(obj, sub2);
+
+            sb = (subject) obj;
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sb;
+    }
+
+    static class student {
+        String name;
+        int rollno;
+        void display() {
+            System.out.println("Name: " + name);
+            System.out.println("Rollno: " + rollno);
+        }
+    }
+
+    static class subject {
+        String sub1, sub2;
+        void display() {
+            System.out.println("Subject1: " + sub1);
+            System.out.println("Subject2: " + sub2);
+        }
+    }
+}
