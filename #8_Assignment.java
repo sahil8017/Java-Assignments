65ac0e6204db220e49b4d650b26abc827f6dae1a
Sahil Gupta <113085266+sahil8017@users.noreply.github.com>
2025-03-04 22:53:48 +0530

    #8_Assignment.java

---
+++ #8_Assignment.java
@@ -0,0 +1,13 @@
+// Write a Java program to calculate the average of values from three months.  
+
+public class average {
+    public static void main(String args[]) {
+        int april = 12, may = 14, june = 8;
+        double average = (april + may + june) / 3.0;
+
+        System.out.println("Synsets Entered for April: " + april);
+        System.out.println("Synsets Entered for May: " + may);
+        System.out.println("Synsets Entered for June: " + june);
+        System.out.println("Synsets Entered for Average: " + average);
+    }
+}
