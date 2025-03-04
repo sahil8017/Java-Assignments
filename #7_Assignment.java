0a3f3160c6f1afb9acf362983d3d75b9e552dc70
Sahil Gupta <113085266+sahil8017@users.noreply.github.com>
2025-03-04 22:47:42 +0530

    #7_Assignment.java

---
+++ #7_Assignment.java
@@ -0,0 +1,11 @@
+// Write a Java program to implement an array and display its elements.  
+
+public class arrayimplementation {
+    public static void main(String args[]) {
+        int arr[] = {1, 2, 3, 4, 5, 6}; 
+
+        for (int num : arr) {
+            System.out.print(num + " ");
+        }
+    }
+}
