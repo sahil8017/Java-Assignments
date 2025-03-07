7fda2d311dc3692c6f34d3c0c2c9afcba399b698
Sahil Gupta <113085266+sahil8017@users.noreply.github.com>
2025-03-07 13:12:30 +0530

    #12_Assignment.java

--- #12_Assignment.java
+++ #12_Assignment.java
@@ -13,3 +13,35 @@ Problem Statement:
 4.6. 30-49: Difficult
 4.7. 0-29: Very Confusing
 */
+import java.util.Scanner;
+
+class ReadabilityAnalyzer {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+
+        System.out.println("Enter a paragraph: ");
+        String text = s.nextLine();
+        s.close();
+
+        int totalWords = text.split("\\s+").length;
+        int totalSentences = text.split("[.!?]+").length;
+        int totalSyllables = countSyllables(text);
+
+        double readabilityScore = 206.835 - (1.015 * totalWords / totalSentences)
+                - (84.6 * totalSyllables / totalWords);
+
+        System.out.println("\nTotal Words: " + totalWords);
+        System.out.println("Total Sentences: " + totalSentences);
+        System.out.println("Total Syllables: " + totalSyllables);
+        System.out.println("Readability Score: " + readabilityScore);
+    }
+
+    static int countSyllables(String text) {
+        String[] words = text.split("\\s+");
+        int totalSyllables = 0;
+        for (String word : words) {
+            totalSyllables += word.replaceAll("[^aeiouAEIOU]", "").length();
+        }
+        return totalSyllables;
+    }
+}
